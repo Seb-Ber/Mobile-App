de56f43e01a047e05fbf233d1a5daa759ef03188
Seber <seb.ber.net@hotmail.fr>
2019-12-09 13:12:29 +0100

    test1

---
+++ app/src/main/java/com/vogella/android/worlds2019/HomeFragment.java
@@ -0,0 +1,16 @@
+package com.vogella.android.worlds2019;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
